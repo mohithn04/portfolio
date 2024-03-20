17ba508d0a74574300a9f881a88aad8ba07b3453
Mohith Nagendra <mohith.n2022@gmail.com>
2024-03-20 01:08:59 -0400

    main test

--- static/js/main.js
+++ static/js/main.js
@@ -1,139 +1,67 @@
-(function() {
-    "use strict";
-  
-    /**
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-      el = el.trim()
-      if (all) {
-        return [...document.querySelectorAll(el)]
-      } else {
-        return document.querySelector(el)
-      }
+/**
+ * Back to top button
+ */
+let backtotop = select('.back-to-top')
+if (backtotop) {
+    const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+    } else {
+        backtotop.classList.remove('active')
     }
-  
-    /**
-     * Easy event listener function
-     */
-    const on = (type, el, listener, all = false) => {
-      let selectEl = select(el, all)
-      if (selectEl) {
-        if (all) {
-          selectEl.forEach(e => e.addEventListener(type, listener))
-        } else {
-          selectEl.addEventListener(type, listener)
-        }
-      }
     }
-  
-    /**
-     * Easy on scroll event listener 
-     */
-    const onscroll = (el, listener) => {
-      el.addEventListener('scroll', listener)
-    }
-  
-    /**
-     * Navbar links active state on scroll
-     */
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-      let position = window.scrollY + 200
-      navbarlinks.forEach(navbarlink => {
-        if (!navbarlink.hash) return
-        let section = select(navbarlink.hash)
-        if (!section) return
-        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-          navbarlink.classList.add('active')
-        } else {
-          navbarlink.classList.remove('active')
-        }
-      })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
-  
-    /**
-     * Scrolls to an element with header offset
-     */
-    const scrollto = (el) => {
-      let elementPos = select(el).offsetTop
-      window.scrollTo({
-        top: elementPos,
-        behavior: 'smooth'
-      })
-    }
-  
-    /**
-     * Back to top button
-     */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-      const toggleBacktotop = () => {
-        if (window.scrollY > 100) {
-          backtotop.classList.add('active')
-        } else {
-          backtotop.classList.remove('active')
-        }
-      }
-      window.addEventListener('load', toggleBacktotop)
-      onscroll(document, toggleBacktotop)
-    }
-  
-    /**
-     * Preloader
-     */
-    let preloader = select('#preloader');
-    if (preloader) {
-      window.addEventListener('load', () => {
-        preloader.remove()
-      });
-    }
-  
-    /**
-     * Hero type effect
-     */
-  
-    /*
-    const typed = select('.typed')
-    if (typed) {
-      let typed_strings = typed.getAttribute('data-typed-items')
-      typed_strings = typed_strings.split(',')
-      new Typed('.typed', {
-        strings: typed_strings,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000,
-        loop: false;
-      });
-    } */
-  
-    var typed = new Typed('#typed', {
-      stringsElement: '#typed-strings',
-      startDelay: 800,
-      typeSpeed: 150,
-      smartBackspace: true,
-      loop: false,
-      onComplete: function () {
-        $('.typed-cursor').delay(1500).animate({fontSize: '0px', lineHeight: '0px', opacity:'0' }, 400);
-      }
-      // onComplete: function(self) { self.cursor.remove() }
-    });
-  
-    /**
-     * Animation on scroll
-     */
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+}
+
+/**
+ * Preloader
+ */
+let preloader = select('#preloader');
+if (preloader) {
     window.addEventListener('load', () => {
-      AOS.init({
-        duration: 1000,
-        easing: 'ease-in-out',
-        once: true,
-        mirror: false
-      })
+    preloader.remove()
     });
-  })()
-  
-  $(document).ready(function() {
-      $('.attribution').modaal();
-  })
+}
+
+/**
+ * Hero type effect
+ */
+
+/*
+const typed = select('.typed')
+if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+    strings: typed_strings,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: false;
+    });
+} */
+
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    startDelay: 800,
+    typeSpeed: 150,
+    smartBackspace: true,
+    loop: false,
+    onComplete: function () {
+    $('.typed-cursor').delay(1500).animate({fontSize: '0px', lineHeight: '0px', opacity:'0' }, 400);
+    }
+    // onComplete: function(self) { self.cursor.remove() }
+});
+
+/**
+ * Animation on scroll
+ */
+window.addEventListener('load', () => {
+    AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+    })
+});
